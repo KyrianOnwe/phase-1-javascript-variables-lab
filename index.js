1e0d3da76392fc82106c8a51abf15aea62a8bcf3
Kyrian <dr.kyrian.onwe@gmail.com>
2022-02-24 22:27:45 -0600

    commit tree cfa11b9412088012c3d3d8fd2e091cb20f055b96

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 // Code your solution in this file!
-const companyName = "Scuber";
+/*const companyName = "Scuber";
 let mostProfitableNeighborhood = "Chelsea";
-let companyCeo = "Susan Smith";
+let companyCeo = "Susan Smith";*/
+const companyName =  `Scuber`;
+let mostProfitableNeighborhood = `Chelsea`;
+let companyCeo = 'Susan Smith';
